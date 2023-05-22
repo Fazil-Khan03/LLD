94a2e838cc612cd97049bdb86482bd62b9caab94
Fazil Khan <fazilkhan0399@gmail.com>
2023-05-22 23:36:37 +0530

    completed Vehicle example

--- vehicle_strategy.py
+++ vehicle_strategy.py
@@ -9,8 +9,36 @@ class DriveStrategy(ABC):
 class NormalDriveStrategy(DriveStrategy):
 
     def drive(self):
+        print("Normal Drive strategy")
+
+class SpecialDriveStrategy(DriveStrategy):
+
+    def drive(self):
+        print("Special Drive strategy")
 
 class Vehicle:
+
+    def __init__(self, drive_request) -> None:
+        self.drive_request = drive_request
+    
+    def drive(self):
+        self.drive_request.drive()
+
+class SportVechicle(Vehicle):
     pass
+
+class NormalVehcile(Vehicle):
+    pass
+
+
+# sport vechicle
+
+sport_vehcile = SportVechicle(SpecialDriveStrategy())
+sport_vehcile.drive()
+
+normal_vehcile = SportVechicle(NormalDriveStrategy())
+normal_vehcile.drive()
+
+
 
     
